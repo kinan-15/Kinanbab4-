37dec7c148f854aa58fa4fb40ccdc69b5790938f
kinan-15 <kenanvidia@gmail.com>
2025-08-06 12:37:50 +0700

    Add files via upload

---
+++ KinanBab4.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kinanbab4;
+
+/**
+ *
+ * @author PC 32 - LAB R1
+ */
+public class KinanBab4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //        String var1 = "1234567899";
+//        String var2 = "3.14";
+//        String var3 = "1";
+//
+//        int bulat = Integer.parseInt(var1);
+//        double pecahan = Double.parseDouble(var2);
+//        short bulat2 = Short.parseShort(var3);
+//
+//        System.out.println("variabel var1 " + var1);
+//        System.out.println("variabel var2 " + var2);
+//        System.out.println("variabel var3 " + var3);
+//        System.out.println("---");
+//        System.out.println("konversi bilangan bulat " + bulat);
+//        System.out.println("konversi bilangan pecahan " + pecahan);
+//        System.out.println("konversi bilangan bulat2 " + bulat2);
+
+//        int var1 = 5;
+//        int var2 = 3;
+//
+//        System.out.println("Hasil : " + var1 / var2);
+
+//        int var1 = 5;
+//        int var2 = 3;
+//
+//        System.out.println("Hasil : " + (double) var1
+//                / var2);
+    }
+    
+}
